8fea2d5cf4b7df66817680e58300b567c13d78ec
CRG2003 <jakecraig50@gmail.com>
2024-06-10 12:49:30 +0100

    Initial commit adding project

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody rb;
+    PlayerInput inp;
+    Animator ani;
+
+    Vector2 dir;
+    float speed = 4;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+        inp = GetComponent<PlayerInput>();
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = inp.actions.FindAction("Movement").ReadValue<Vector2>();
+        ani.SetFloat("x", dir.x);
+        ani.SetFloat("y", dir.y);
+
+        rb.AddForce(new Vector3(dir.x, 0, dir.y * 2) * speed);
+    }
+}
