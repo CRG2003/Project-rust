fc6413f4c1ca728ad3bc637396f84b4335794577
CRG2003 <jakecraig50@gmail.com>
2024-06-18 10:51:52 +0100

    added field placement

---
+++ Assets/Scripts/grid.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    IDictionary<Vector2, string> points = new Dictionary<Vector2, string>();
+
+    public GameObject plot;
+
+    public bool createPlot(Vector2 pos, string type){
+        if (points.ContainsKey(pos)){
+            Debug.Log("Plot already exists");
+            return false;
+        }
+        else{
+            points.Add(pos, type);
+            Instantiate(plot, new Vector3(pos.x, 0, pos.y), Quaternion.identity);
+            return true;
+        }
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,27 +5,42 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    public GameObject orb;
+
     Rigidbody rb;
     PlayerInput inp;
     Animator ani;
 
     Vector2 dir;
-    float speed = 4;
+    float speed = 6;
+
+    grid scr;
 
     void Start()
     {
         rb = transform.GetComponent<Rigidbody>();
         inp = GetComponent<PlayerInput>();
         ani = GetComponent<Animator>();
+
+        scr = GetComponent<grid>();
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         dir = inp.actions.FindAction("Movement").ReadValue<Vector2>();
         ani.SetFloat("x", dir.x);
         ani.SetFloat("y", dir.y);
 
-        rb.AddForce(new Vector3(dir.x, 0, dir.y * 2) * speed);
+        rb.AddForce(new Vector3(dir.x, 0, dir.y * 1.5f) * speed);
+
+        int a = (int)(rb.transform.position.x) / 3;
+        int b = (int)(rb.transform.position.z) / 3;
+
+        orb.transform.position = new Vector3(a*3, 0, b*3);
+
+        if (inp.actions.FindAction("e").ReadValue<float>() > 0){          
+            scr.createPlot(new Vector2(a*3, b*3), "field");
+        }
     }
 }
