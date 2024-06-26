848aac2ddd4bcca4a818672bdc161b3b988dd09c
unknown <jakecraig50@gmail.com>
2024-06-26 13:28:11 +0100

    Added basis of inventory system

--- Assets/Scripts/items.cs
+++ Assets/Scripts/items.cs
@@ -2,12 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// classes to access items
+public class items : MonoBehaviour { 
+    public item returnExample(){ return new example(); }
+    public item returnApple(){ return new apple(); }
+}
+
+
+// item base class
 public class item
 {
-    string name;
+    protected string name;
+    protected int count = 0;
+    protected int capacity;
+
+    public string getName() { return name; }
+    public int getCapacity() { return capacity; }
+    public int getCount() { return count; }
 }
 
-public class hoe : item { 
 
+
+// List of actual items
+public class example : item { 
+    public example() {
+        name = "example";
+        count = 1;
+        capacity = 5;
+    }
 }
 
+public class apple : item {
+    public apple() {
+        name = "Apple";
+        count = 1;
+        capacity = 10;
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,31 +6,41 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+
+    // Players components
     public GameObject orb;
 
     Rigidbody rb;
     PlayerInput inp;
     Animator ani;
 
+    GameObject t;
+    grid gri;
+    items it;
+
+    // Player variables 
     Vector2 dir;
     float speed = 6;
 
-    grid scr;
+    public IDictionary<item, int> inventory = new Dictionary<item, int>();
 
-    //IDictionary<int, >
 
-    void Start()
-    {
+
+    void Start() {
+
         rb = transform.GetComponent<Rigidbody>();
         inp = GetComponent<PlayerInput>();
         ani = GetComponent<Animator>();
 
-        scr = GetComponent<grid>();
+        t = GameObject.Find("Things");
+        gri = t.GetComponent<grid>();
+        it = t.GetComponent<items>();   
     }
 
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
+
+        // Player inputs
         dir = inp.actions.FindAction("Movement").ReadValue<Vector2>();
         ani.SetFloat("x", dir.x);
         ani.SetFloat("y", dir.y);
@@ -39,10 +50,34 @@ public class playerController : MonoBehaviour
         int a = (int)(rb.transform.position.x) / 3;
         int b = (int)(rb.transform.position.z) / 3;
 
-        orb.transform.position = new Vector3(a*3, 0, b*3);
+        orb.transform.position = new Vector3(a * 3, 0, b * 3);
 
-        if (inp.actions.FindAction("e").ReadValue<float>() > 0){          
-            scr.createPlot(new Vector2(a*3, b*3), "field");
+        if (inp.actions.FindAction("e").ReadValue<float>() > 0) {
+            gri.createPlot(new Vector2(a * 3, b * 3), "field");
+        }
+
+        if (inp.actions.FindAction("q").ReadValue<float>() > 0) {
+            inventory.Add(it.returnExample(), 1);
+        }
+
+
+        // debugging
+        int i = 0;
+        foreach (KeyValuePair<item, int> k in inventory) {
+            i++;
+            Debug.Log(i + ": " + k.Key.getName() + " - " + k.Key.getCount());
+        }
+
+    }
+    void OnTriggerEnter(Collider hit) {
+        if (hit.CompareTag("apple")) {
+            if (inventory.ContainsKey(it.returnApple())){
+                inventory[it.returnApple()]++;
+            }
+            else {
+                inventory.Add(it.returnApple(), 1);
+            }
+            Destroy(hit.gameObject);
         }
     }
 }
